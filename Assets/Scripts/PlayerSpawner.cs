5225c8b4ffbc1c5a5581cc7b50fded4f909d0a18
Nikhil Dahake <nikhil.dahake@blackmarchstudios.com>
2023-05-30 19:27:55 +0530

    --> Photon Setup

---
+++ Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class PlayerSpawner : MonoBehaviourPunCallbacks
+{
+    public GameObject playerPrefab;
+   // public GameObject[] playerPrefab1;
+   // public Transform[] spawnPoints;
+    //public TMP_Text pingRateText;
+
+    void Start()
+    {
+       // int randomIndex = Random.Range(0, spawnPoints.Length);
+      //  int randomIndexPlayerPref = Random.Range(0, playerPrefab1.Length);
+       // pingRateText.text = "Network Ping : " + PhotonNetwork.GetPing();
+        if (PhotonNetwork.IsConnected)
+        {
+            // Create the player object across the network
+            // PhotonNetwork.Instantiate(playerPrefab.name, spawnPoints[PhotonNetwork.LocalPlayer.ActorNumber - 1].position, Quaternion.identity);
+            // PhotonNetwork.Instantiate(playerPrefab.name, spawnPoints[randomIndex].position, Quaternion.identity);
+            PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(0,0,0), Quaternion.identity);
+        }
+    }
+}
--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
+using Photon.Pun;
+using Photon.Realtime;
 
 public class ThirdPersonController : MonoBehaviour
 {
@@ -8,42 +11,53 @@ public class ThirdPersonController : MonoBehaviour
     public float moveSpeed;
     public Transform cam;
     Animator anim;
-
+    public PhotonView pv;
+    public CinemachineVirtualCamera vcCam;
     void Start()
     {
+        vcCam = FindObjectOfType<CinemachineVirtualCamera>();
         characterController = GetComponent<CharacterController>();
         anim =gameObject. GetComponent<Animator>();
+
+
+        if (pv.IsMine)
+        {
+            vcCam.Follow = this.gameObject.transform;
+        }
     }
 
     void FixedUpdate()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        if (pv.IsMine)
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
 
-        Vector3 movementInput = new Vector3(horizontalInput, 0, verticalInput);
-        Vector3 movementDirection = movementInput.normalized;
+            Vector3 movementInput = new Vector3(horizontalInput, 0, verticalInput);
+            //  Vector3 movementDirection = movementInput.normalized;
 
-        if(horizontalInput>=1 )
-        {
-            anim.SetTrigger("RightWalk");
-        }
-        else if ( horizontalInput <=-1)
-        {
-            anim.SetTrigger("LeftWalk");
-        }    
-        else if (verticalInput >= 1)
-        {
-            anim.SetTrigger("FrontWalk");
-        }
-        else if (verticalInput <= -1)
-        {
-            anim.SetTrigger("BackWalk");
-        }
-        else
-        {
-            anim.SetTrigger("Idle");
-        }
-      
-        characterController.Move(movementDirection * moveSpeed * Time.deltaTime);
+            if (horizontalInput >= 1)
+            {
+                anim.SetTrigger("RightWalk");
+            }
+            else if (horizontalInput <= -1)
+            {
+                anim.SetTrigger("LeftWalk");
+            }
+            else if (verticalInput >= 1)
+            {
+                anim.SetTrigger("FrontWalk");
+            }
+            else if (verticalInput <= -1)
+            {
+                anim.SetTrigger("BackWalk");
+            }
+            else
+            {
+                anim.SetTrigger("Idle");
+            }
+
+            characterController.Move(movementInput * moveSpeed * Time.deltaTime);
+        }        
     }
 }
